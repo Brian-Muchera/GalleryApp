9b557e3b6b46e5cd37993e9f9477b440d9fc346b
Brian-Muchera <mucherabrian2@gmail.com>
2020-10-12 17:06:34 +0300

    deploy

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,36 +11,36 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-#import django_heroku
-#import dj_database_url
-#from decouple import config,Csv
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
-#MODE=config("MODE", default="dev")
+MODE=config("MODE", default="dev")
 
-#DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 # development
-#if config('MODE')=="dev":
-  # DATABASES = {
-  #     'default': {
-  #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   #        'NAME': config('DB_NAME'),
-   #        'USER': config('DB_USER'),
-   #        'PASSWORD': config('DB_PASSWORD'),
-   #        'HOST': config('DB_HOST'),
-  #         'PORT': '',
-  #     }
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
        
-  # }
+   }
 # production
-#else:
- #  DATABASES = {
-    #   'default': dj_database_url.config(
-    #       default=config('DATABASE_URL')
-   #    )
-  # }
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
 
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -169,5 +169,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
